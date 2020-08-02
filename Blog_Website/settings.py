3fe3d87a1beca91ded75003695965f70771d5c82
Cyborg_7898 <www.smitraj333@gmail.com>
2020-08-02 10:00:42 +0530

    Chnages Uploaded

--- Blog_Website/settings.py
+++ Blog_Website/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import environ
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -127,6 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles') #To push on heroku
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
@@ -134,3 +136,4 @@ STATICFILES_DIRS=[
 CRISPY_TEMPLATE_PACK="bootstrap4"
 LOGIN_REDIRECT_URL="home"
 LOGIN_URL="login"
+django_heroku.settings(locals())
